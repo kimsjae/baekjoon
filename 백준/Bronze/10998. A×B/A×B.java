3dc255b99cafdc37fda0ce0d9beff58b35cff65c
kimsjae <153582301+kimsjae@users.noreply.github.com>
2024-08-29 14:58:42 +0900

    [Bronze V] Title: A×B, Time: 64 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		String split[] = s.split(" ");
+		int num1 = Integer.valueOf(split[0]);
+		int num2 = Integer.valueOf(split[1]);
+		
+		if (num1 < 0 || num1 > 10 || num2 < 0 || num2 > 10) {
+			bw.write("0 < A, B < 10\n");
+			bw.flush();
+			bw.close();
+		} else {
+			int product = num1 * num2;
+			
+			bw.write(product + "\n");
+			bw.flush();
+			bw.close();
+		}
+	}
+
+}
