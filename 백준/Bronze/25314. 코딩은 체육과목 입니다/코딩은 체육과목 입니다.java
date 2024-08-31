fe76fab3f411937f42bdc518336c39dc8672b7e7
kimsjae <153582301+kimsjae@users.noreply.github.com>
2024-09-01 04:08:39 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 64 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -9,8 +9,28 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int num = Integer.parseInt(br.readLine());
+		
+		// 정수 N 입력 (4 <= N <= 1,000; N은 4의 배수)
+		int num;
+		
+		// N 예외처리
+		while(true) {
+			num = Integer.parseInt(br.readLine());
+			
+			if (
+				num < 4 || 
+				num > 1000 ||
+				num % 4 != 0
+				) {
+					bw.write("4 <= N <= 1,000; N은 4의 배수\n");
+					bw.write("다시 정수 N을 입력해주세요.\n");
+					bw.flush();
+			} else {
+				break;
+			}
+		}
+		
+		// long 개수
 		int longCount = num / 4;
 		
 		for (int i = 0; i < longCount; i++) {
