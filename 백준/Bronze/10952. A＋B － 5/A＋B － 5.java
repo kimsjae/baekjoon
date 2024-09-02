f680ea0df06b488e9ff06903dccf17c248c47ade
kimsjae <153582301+kimsjae@users.noreply.github.com>
2024-09-02 12:22:10 +0900

    [Bronze V] Title: A+B - 5, Time: 64 ms, Memory: 11640 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		while(true) {
+			String s[] = br.readLine().split(" ");
+			int a = Integer.parseInt(s[0]);
+			int b = Integer.parseInt(s[1]);
+			if (
+				a < 0 || a >= 10 ||
+				b < 0 || b >= 10
+				) {
+				bw.write("0 < A, B < 10\n");
+				bw.flush();
+				bw.close();
+				return;
+			}
+			
+			if (a == 0 && b == 0) {
+				break;
+			} else {
+				int c = a + b;
+				bw.write(c + "\n");
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+
+}
